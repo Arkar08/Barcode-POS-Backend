bb0487a107480c9fa5b060e0529f1cb0641419c1
uiux@okdollar frontend <uiux@okdollar.com>
2025-04-09 10:41:05 +0630

    fix

--- src/services/findService.js
+++ src/services/findService.js
@@ -1,26 +1,26 @@
 import { db } from "../server.js";
 
-export const findSupplierService = async () => {
-  let dataPass = [];
-  await db
-    .query(
-      "SELECT userId,fullName,email,password,roleName,companyName,state,township,address,description FROM USER LEFT JOIN ROLE ON USER.roleId = ROLE.roleId LEFT JOIN INFORMATION ON user.informationId = information.informationId WHERE roleName = 'supplier'"
-    )
-    .then((data) => {
-      return (dataPass = data[0]);
-    })
-    .catch((error) => {
-      console.log(error, "find Supplier db error is");
-    });
-  return dataPass;
-};
-
-
-export const findCustomerService = async () => {
+  export const findSupplierService = async () => {
     let dataPass = [];
     await db
       .query(
-        "SELECT userId,fullName,email,password,roleName,companyName,state,township,address,description FROM USER LEFT JOIN ROLE ON USER.roleId = ROLE.roleId LEFT JOIN INFORMATION ON user.informationId = information.informationId WHERE roleName = 'customer'"
+        "SELECT * FROM USER  WHERE roleId = 3"
+      )
+      .then((data) => {
+        return (dataPass = data[0]);
+      })
+      .catch((error) => {
+        console.log(error, "find Supplier db error is");
+      });
+    return dataPass;
+  };
+
+
+  export const findCustomerService = async () => {
+    let dataPass = [];
+    await db
+      .query(
+        "SELECT * FROM USER  WHERE roleId = 2"
       )
       .then((data) => {
         return (dataPass = data[0]);
